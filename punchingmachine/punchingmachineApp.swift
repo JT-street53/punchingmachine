6a31d66851c2f0d5e7e69127f39ef08ae53c33c7
JT-Street53 <kit.kat.bar86400@gmail.com>
2021-12-01 16:01:26 -0800

    Initial Commit

---
+++ punchingmachine/punchingmachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  punchingmachineApp.swift
+//  punchingmachine
+//
+//  Created by Shelshoker on 2021/12/01.
+//
+
+import SwiftUI
+
+@main
+struct punchingmachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
